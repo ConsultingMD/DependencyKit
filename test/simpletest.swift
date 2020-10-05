9d4abc1daab2882be8b80f723b460bc503be443f
Adam Zethraeus <adam.zethraeus@grandrounds.com>
2020-10-05 10:53:56 -0700

    trying for only autogenning for protocol

--- test/simpletest.swift
+++ test/simpletest.swift
@@ -2,8 +2,6 @@ import Foundation
 
 // INTERFACE
 
-protocol Provisions {}
-
 protocol Speaking: Provisions {
     func say(_ string: String)
 }
@@ -12,45 +10,68 @@ protocol Thinking: Provisions {
     var thoughts: String { get }
 }
 
+// FRAMEWORK
+
+protocol Component {
+    associatedtype Requirements 
+}
+
+protocol Provisions {}
+
+protocol ComponentProvisions: Component, Provisions {}
+
+
+class NilComponentProvisions: ComponentProvisions {
+    typealias Requirements = NilComponentProvisions
+    lazy var requirements = self
+}
+
+class ComponentObject<Dep: ComponentProvisions>: Component {
+    typealias Requirements = Dep
+    let requirements: Requirements
+    init(requirements: Dep) {
+        self.requirements = requirements
+    }
+}
+
 // IMPL
 
-class Person: Speaking {
-    func say(_ string: String) {
-        print("~whispering~: \(string)")
-    }
-}
+// class Person: ComponentObject, Speaking {
+//     func say(_ string: String) {
+//         print("~whispering~: \(string)")
+//     }
+// }
 
-class YellingPerson: Speaking {
-    func say(_ string: String) {
-        print("*shouting*: \(string.uppercased())")
-    }
-}
+// class YellingPerson: ComponentObject, Speaking {
+//     func say(_ string: String) {
+//         print("*shouting*: \(string.uppercased())")
+//     }
+// }
 
-class Test<Requirements: Provisions>: Thinking {
+class Test: ComponentObject<NilComponentProvisions>, Thinking {
     let id = UUID()
     let thoughts: String
-    let requirements: Requirements
-    init(requirements: Requirements, thoughts: String) {
+    init(requirements: NilComponentProvisions, thoughts: String) {
         self.thoughts = thoughts
-        self.requirements = requirements
+        super.init(requirements: requirements)
     }
 }
 
 // AUTOGEN EXTENSIONS
 
-extension Thinking where Self == Test<Person> {
-    var requirements: Person { requirements }
-    func say(_ string: String) { requirements.say(string) }
-}
+// extension Thinking where Self: Component, Component.Requirements == Person {
+//     var requirements: Person { requirements }
+//     func say(_ string: String) { requirements.say(string) }
+// }
 
-extension Thinking where Self == Test<YellingPerson> {
-    var requirements: YellingPerson { requirements }
-    func say(_ string: String) { requirements.say(string) }
-}
+// extension Thinking where Self == Test<YellingPerson> {
+//     var requirements: YellingPerson { requirements }
+//     func say(_ string: String) { requirements.say(string) }
+// }
 
 // USAGE
 
-let baseTest = Test(requirements: Person(), thoughts: "i'm pretty normal.")
-let altTest = Test(requirements: YellingPerson(), thoughts: "i'm exactly the same as them.")
-baseTest.say(baseTest.thoughts)
-altTest.say(altTest.thoughts)
+// let baseTest = Test(requirements: Person(), thoughts: "i'm pretty normal.")
+// let altTest = Test(requirements: YellingPerson(), thoughts: "i'm exactly the same as them.")
+// baseTest.say(baseTest.thoughts)
+// altTest.say(altTest.thoughts)
