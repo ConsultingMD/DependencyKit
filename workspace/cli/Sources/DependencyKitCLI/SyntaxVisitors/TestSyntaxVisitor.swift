04f1004a303336401cbe99ee05465c63fb841cc3
Adam Zethraeus <adam@zethrae.us>
2021-01-22 08:21:11 -0800

    get class info

--- workspace/cli/Sources/DependencyKitCLI/SyntaxVisitors/TestSyntaxVisitor.swift
+++ workspace/cli/Sources/DependencyKitCLI/SyntaxVisitors/TestSyntaxVisitor.swift
@@ -20,8 +20,8 @@ class TestSyntaxVisitor: SyntaxVisitor {
     override func visit(_ token: ProtocolDeclSyntax) -> SyntaxVisitorContinueKind {
         // For both Dependency & Requirement
         print("""
-        %%% PROTOCOL INHERITANCE %%%
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%% PROTOCOL %%%
+        %%%%%%%%%%%%%%%%
 
         (access) modifiers: >"\(String(describing:token.modifiers?.children.flatMap{$0.children.flatMap{$0.tokens.map{$0.text}}}))"<
 
@@ -31,7 +31,25 @@ class TestSyntaxVisitor: SyntaxVisitor {
         return super.visit(token)
     }
     
-    // TODO: Subclasses & their parents
+    override func visit(_ token: ClassDeclSyntax) -> SyntaxVisitorContinueKind {
+        // For both Dependency & Requirement
+        print("""
+        %%% CLASS %%%
+        %%%%%%%%%%%%%
+
+        (access) modifiers: >"\(String(describing:token.modifiers?.children.flatMap{$0.children.flatMap{$0.tokens.map{$0.text}}}))"<
+
+        generic: >"\(String(describing:
+                                token.genericParameterClause?.genericParameterList.children.flatMap { $0.tokens.map{ $0.text }}
+                        ))"<
+
+        inheritanceClause: >"\(String(describing: token.inheritanceClause?.inheritedTypeCollection.flatMap{$0.children.flatMap{$0.children.flatMap {$0.tokens.map{$0.text}}}}))"<
+
+        """)
+        return super.visit(token)
+    }
+    
+    // TODO: Subclasses & their parents //ClassDecl
     
     // TODO: Instantiations & their parameters.
     // Finding the type of the param might be an issue?
