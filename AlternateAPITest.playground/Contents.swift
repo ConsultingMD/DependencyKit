2a8cabdefb68b932b05c96bfa81051a6c37eb0ef
Adam Zethraeus <adam@zethrae.us>
2021-03-07 20:00:18 -1000

    delete alt api playground

--- AlternateAPITest.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-let root = RootResource(injecting: NilResource())
-let levelOne = root.levelOneResource
-let levelTwo = levelOne.levelTwoResource
-print(levelTwo.explicitPassthrough)
-print(levelTwo.modified)
-print(levelTwo.recreated)
-//print(levelTwo.implicitPassthrough)
