581323225f544c529771eeba75ffe6c2b6b8ffa7
Adam Zethraeus <adam@zethrae.us>
2021-03-20 17:48:26 -0700

    remove xproj .gitignore

--- Tests/DependencyKitTests/DependencyKitTests.swift
+++ Tests/DependencyKitTests/DependencyKitTests.swift
@@ -6,7 +6,6 @@ final class DependencyKitTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(DependencyKit().text, "Hello, World!")
     }
 
     static var allTests = [
