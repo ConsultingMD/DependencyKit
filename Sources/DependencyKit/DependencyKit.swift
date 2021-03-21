a8783a85c952d3ad9ccb70aeb5adc926c3999c0f
Adam Zethraeus <adam@zethrae.us>
2021-03-21 01:37:59 -0700

    add runtime params

--- Sources/DependencyKit/DependencyKit.swift
+++ Sources/DependencyKit/DependencyKit.swift
@@ -8,12 +8,21 @@ public protocol NilRequirements: Requirements {}
 
 public protocol ResourceType: Requirements {}
 
-open class Resource<I: Requirements>: ResourceType {
+open class Resource<I: Requirements, P>: ResourceType {
     public let injected: I
-    public init(injecting injected: I) {
+    public let parameters: P
+    public init(injecting injected: I, parameters: P) {
         self.injected = injected
+        self.parameters = parameters
     }
 }
+
+public extension Resource where P == Void {
+    convenience init(injecting injected: I) {
+        self.init(injecting: injected, parameters: ())
+    }
+}
+
 public class NilResource: ResourceType, NilRequirements {
     public lazy var injected = self
     public init(){}
